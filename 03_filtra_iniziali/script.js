b090a2e321d6b120957a1c9c0fc960bc7fbbcbc4
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-11 18:23:35 +0100

    complete exercise folder 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,11 +2,34 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+//dichiaro la lettera (l'iniziale)
+const letter="M";
 
 // Dichiara la funzione qui.
+function initialFilter (arrNames, initialLetter){
+    //dichiaro l'array che conterrà le parole filtrate per iniziale
+    const filteredArr=[];
+    console.log( filteredArr);
+
+    //scorro l'array ricevuto come parametro
+    for (let i=0; i<arrNames.length; i++){
+        //salvo l'elemento dell'array
+        const thisName=arrNames[i];
+        //Confronto l'iniziale del nome thisName (elemento posizione 0) con la lettera ricevuta come parametro
+        if (thisName[0] ===initialLetter ){
+            //aggiungo l'elemento all'array
+            filteredArr.push(thisName);
+        }
+    }
+    return filteredArr;  
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const arrayFiltered= initialFilter (names, letter);
+console.log(arrayFiltered);
+
 
 
 
