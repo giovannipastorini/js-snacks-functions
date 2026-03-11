425818fb1a03a154083db47c4ac75d9af48b9b43
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-11 18:50:38 +0100

    complete exercise folder 04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,12 +1,51 @@
+console.log("snack 4");
+
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+const word = 'javascriptaeiou';
 
 
 // Dichiara la funzione qui.
+function countVocal(wordA){
+    //inizializzo il contatore delle vocali
+    let numVocals=0;
+    for(let i=0; i<wordA.length; i++){
+        const thisChar=wordA[i];
+        //usa "switch" per fare un controllo su "thisChar" e vedere se si tratta di una vocale
+        switch(thisChar) {
+            case "a":
+                // code block
+                numVocals +=1;
+                break;
+            case "e":
+                // code block
+                numVocals +=1;
+                break;
+            case "i":
+                // code block
+                numVocals +=1;
+                break;
+            case "o":
+                // code block
+                numVocals +=1;
+                break;
+            case "u":
+                // code block
+                numVocals +=1;
+                break;
+            /* default:
+                // code block */
+        }
+    }
+    //restituisco il contatore
+    return numVocals;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const vocals=countVocal(word);
+console.log(vocals);
+
 
 
 
